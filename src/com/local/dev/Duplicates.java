8d8fbe05a86b539d4e63fe7ab45c76a357d89f61
srinivv <srinivv@gmail.com>
2020-08-09 16:03:54 -0500

    testing commit

--- src/com/local/dev/Duplicates.java
+++ src/com/local/dev/Duplicates.java
@@ -5,7 +5,7 @@ public class Duplicates {
 		int[] tempArr = new int[arr.length];
 
 		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) { 
 			int current = arr[i];
 
 			if (count > 0) {
