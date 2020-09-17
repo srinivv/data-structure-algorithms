5681a0ae11c20e76fc010b1412bc2979f222f441
srinivv <srinivv@gmail.com>
2020-09-16 22:38:39 -0500

    Updated

--- src/com/local/ms/RemoveDuplicates.java
+++ src/com/local/ms/RemoveDuplicates.java
@@ -1,24 +1,23 @@
-class RemoveDuplicates 
-{ 
-    /* Function removes duplicate characters from the string 
-    This function work in-place */
-    void removeDuplicates(String str) 
-    { 
-        LinkedHashSet<Character> lhs = new LinkedHashSet<>(); 
-        for(int i=0;i<str.length();i++) 
-            lhs.add(str.charAt(i)); 
-          
-        // print string after deleting duplicate elements 
-        for(Character ch : lhs) 
-            System.out.print(ch); 
-    } 
-      
-    /* Driver program to test removeDuplicates */
-    public static void main(String args[]) 
-    { 
-        String str = "geeksforgeeks"; 
-        RemoveDuplicates r = new RemoveDuplicates(); 
-        r.removeDuplicates(str); 
-    } 
-} 
-  
+package com.local.ms;
+
+import java.util.LinkedHashSet;
+
+// Removes duplicate characters from the string
+
+public class RemoveDuplicates {
+	
+	void removeDuplicates(String str) {
+		LinkedHashSet<Character> lhs = new LinkedHashSet<>();
+		for (int i = 0; i < str.length(); i++)
+			lhs.add(str.charAt(i));
+
+		for (Character ch : lhs)
+			System.out.print(ch);
+	}
+
+	public static void main(String args[]) {
+		String str = "srinivasanvelusamy";
+		RemoveDuplicates r = new RemoveDuplicates();
+		r.removeDuplicates(str);
+	}
+}
