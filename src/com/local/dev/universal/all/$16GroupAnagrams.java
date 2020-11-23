b97768db7135a67d6451be680ce55ff43fc4cfed
srini <srini@home-9300>
2020-11-22 19:11:23 -0600

    updated commons

---
+++ src/com/local/dev/universal/all/$16GroupAnagrams.java
@@ -0,0 +1,42 @@
+package com.local.dev.universal.all;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ */
+public class $16GroupAnagrams {
+
+	public List<List<String>> groupAnagrams(String[] strs) {
+		if (strs.length == 0)
+			return new ArrayList();
+		Map<String, List> ans = new HashMap<String, List>();
+		int[] count = new int[26];
+		for (String s : strs) {
+			Arrays.fill(count, 0);
+			for (char c : s.toCharArray())
+				count[c - 'a']++;
+
+			StringBuilder sb = new StringBuilder("");
+			for (int i = 0; i < 26; i++) {
+				sb.append('#');
+				sb.append(count[i]);
+			}
+			String key = sb.toString();
+			if (!ans.containsKey(key))
+				ans.put(key, new ArrayList());
+			ans.get(key).add(s);
+		}
+		return new ArrayList(ans.values());
+	}
+}
---
+++ src/com/local/dev/universal/all/$22MergeSortedArray.java
@@ -0,0 +1,18 @@
+package com.local.dev.universal.all;
+
+import java.util.Arrays;
+
+/*
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+ */
+public class $22MergeSortedArray {
+
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		System.arraycopy(nums2, 0, nums1, m, n);
+		Arrays.sort(nums1);
+	}
+}
